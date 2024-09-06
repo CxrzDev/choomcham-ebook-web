28a1637bf249e1ce525f0ded6f424685a0255ed4
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2024-09-06 23:02:12 +0700

    update api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 
 export default async function Home() {
    
-    // var res = await fetch(`https://choomcham.vercel.app/api/course`)
+    var res = await fetch(`https://choomcham.vercel.app/api/course`)
     // var result : any = await res.json()
     // var contents :any = [...result.data];
     var contents :any = [];
