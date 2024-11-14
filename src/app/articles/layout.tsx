a62fd473f30e1d234c9d33c75e5ffb4f0e3fe531
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2024-11-14 12:39:57 +0700

    fixed error when build

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -2,6 +2,29 @@ import Navbar from "../components/nav";
 import Footer from '../components/footer';
 import NavigationBar from "../components/navbar";
 
+
+// layout.tsx or page.tsx
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'บทความสร้างแบรนด์ | เทคนิคและแนวคิดจาก Choomcham',
+    description: 'อ่านบทความจาก Choomcham ที่อัดแน่นด้วยเทคนิคการสร้างแบรนด์และกลยุทธ์การตลาด เรียนรู้การสร้างตัวตนให้แข็งแกร่งและดึงดูดลูกค้า ด้วยความรู้และประสบการณ์จากผู้เชี่ยวชาญ',
+    authors: [{ name: 'Choomcham Branding' }],
+    keywords: ['Choomcham', 'บทความการสร้างแบรนด์', 'การสร้างแบรนด์', 'การตลาด', 'บทความการตลาด', 'personal branding', 'กลยุทธ์การตลาด', 'การเล่าเรื่อง', 'สร้างตัวตนแบรนด์', 'Choomcham Branding'],
+    publisher: "Choomcham Branding",
+    robots: { index: true, follow: true },
+    icons: "/images/cropped-logo.png",
+    openGraph: {
+        images: [{
+            url: "https://choomcham.com/images/article-cover.png",
+            alt: 'บทความสร้างแบรนด์และกลยุทธ์การตลาดโดย Choomcham',
+        }]
+    }
+};
+
+// Now you can import the Blog component here or set up the layout.
+
 function layout({
     children, // will be a page or nested layout
   }: {
