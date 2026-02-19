8b98719f4d39ee3376f362b9d8eeb348650780d2
467415241005-st-hub <467415241005-st@rmutsb.ac.th>
2026-02-19 22:41:52 +0700

    Allow static generation and disable broken API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // 'use client'
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
 import { motion } from 'framer-motion'
 import Footer from './components/footer'
